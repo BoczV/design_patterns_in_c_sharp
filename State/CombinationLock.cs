b6819aa691cf0b66bb3f124fd3f67d8bab2f4a78
Bócz Viktor <56642389+BoczV@users.noreply.github.com>
2022-12-03 10:00:25 +0100

    CombinationLock implementation

---
+++ State/CombinationLock.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace State;
+
+public class CombinationLock
+{
+    private string? _code;
+    private string? _status;
+
+    public string Status { get => _status?? "LOCKED"; }
+
+    public CombinationLock(int[] combination) => SetNewCombination(combination);
+
+    public void EnterDigit(int digit)
+    {
+        if(_status == "LOCKED")
+        {
+            _status = "";
+        }
+        _status += digit;
+        if(_status == _code)
+        {
+            _status = "OPEN";
+        }
+        if(_status.Length == _code.Length)
+        {
+            _status = "ERROR";
+        }
+    }
+
+    public void SetNewCombination(int[] combination)
+    {
+        var stringBuilder = new StringBuilder();
+        for (int i = 0; i < combination.Length; i++)
+        {
+            stringBuilder.Append(combination[i]);
+        }
+        _code = stringBuilder.ToString();
+        _status = "LOCKED";
+    }
+}
