1fb8cd9b00e24289cf3392a33553e733091964ec
Bócz Viktor <56642389+BoczV@users.noreply.github.com>
2022-12-03 10:07:16 +0100

    Create demo

--- State/Program.cs
+++ State/Program.cs
@@ -4,6 +4,19 @@ public static class Program
 {
     static void Main()
     {
+        int[] combination = new[] { 1, 2, 3, 4, 5 };
+        var combinationLock = new CombinationLock(combination);
+        OpenCombinationLock(combination, combinationLock);
+    }
 
+    public static void OpenCombinationLock(int[] combination, CombinationLock combinationLock)
+    {
+        Console.WriteLine($"Actual status of combination lock: {combinationLock.Status}");
+
+        for (int i = 0; i < combination.Length; i++)
+        {
+            combinationLock.EnterDigit(combination[i]);
+            Console.WriteLine($"Status: {combinationLock.Status}");
+        }
     }
 }
