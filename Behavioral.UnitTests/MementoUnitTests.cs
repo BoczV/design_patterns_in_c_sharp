2dd752ea84d29bb11dd1df6be8b9cb6eef4ced10
Bócz Viktor <56642389+BoczV@users.noreply.github.com>
2022-12-05 18:42:14 +0100

    Tests added to Memento

---
+++ Behavioral.UnitTests/MementoUnitTests.cs
@@ -0,0 +1,46 @@
+﻿using FluentAssertions;
+using Memento;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Behavioral.UnitTests;
+
+[TestFixture]
+public class MementoUnitTests
+{
+    private BankAccount _bankAccount;
+
+    private string _defaultOwnerName;
+
+    private string _defaultAccountType;
+
+    [SetUp]
+    public void Setup()
+    {
+        _defaultOwnerName = "Erika";
+        _defaultAccountType = "Retail";
+        _bankAccount = new BankAccount(_defaultOwnerName, _defaultAccountType);
+    }
+
+    [Test]
+    public void TestMementoRestore()
+    {
+        // Arrange
+        Guid guid = _bankAccount.Deposit(100);
+        _bankAccount.Deposit(100);
+        _bankAccount.OwnerName = "Károly";
+        _bankAccount.AccountType = "SomethingElse";
+
+        // Act
+        _bankAccount.Reset(guid);
+
+        // Assert
+        _bankAccount.OwnerName.Should().Be(_defaultOwnerName);
+        _bankAccount.AccountType.Should().Be(_defaultAccountType);
+
+    }
+}
