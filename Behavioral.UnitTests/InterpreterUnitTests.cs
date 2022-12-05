44dfd0454ec44eefd5d923d6fa613684c5a07557
Bócz Viktor <56642389+BoczV@users.noreply.github.com>
2022-12-05 17:57:19 +0100

    Tests added to Interpreter

---
+++ Behavioral.UnitTests/InterpreterUnitTests.cs
@@ -0,0 +1,95 @@
+﻿using FluentAssertions;
+using Interpreter;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Behavioral.UnitTests;
+
+[TestFixture]
+public class InterpreterUnitTests
+{
+    private ExpressionProcessor _expressionProcessor;
+
+    [SetUp]
+    public void Setup()
+    {
+        _expressionProcessor = new ExpressionProcessor();
+    }
+
+    [Test]
+    public void TestOperationWithVariable()
+    {
+        // Arrange
+        var variableKey = "x";
+        var variableValue = 100;
+        var strExpression = "2*x";
+
+        // Act
+        _expressionProcessor.AddNewVariable(variableKey, variableValue);
+        _expressionProcessor.GetOneExpression(strExpression);
+        var result = _expressionProcessor.Calculate();
+
+        // Assert
+        result.Should().Be(200);
+    }
+
+    [Test]
+    public void TestAddition()
+    {
+        // Arrange
+        var strExpression = "2+10";
+
+        // Act
+        _expressionProcessor.GetOneExpression(strExpression);
+        var result = _expressionProcessor.Calculate();
+
+        // Assert
+        result.Should().Be(12);
+    }
+
+    [Test]
+    public void TestSubtraction()
+    {
+        // Arrange
+        var strExpression = "200-10";
+
+        // Act
+        _expressionProcessor.GetOneExpression(strExpression);
+        var result = _expressionProcessor.Calculate();
+
+        // Assert
+        result.Should().Be(190);
+    }
+
+    [Test]
+    public void TestMultiplication()
+    {
+        // Arrange
+        var strExpression = "200*2";
+
+        // Act
+        _expressionProcessor.GetOneExpression(strExpression);
+        var result = _expressionProcessor.Calculate();
+
+        // Assert
+        result.Should().Be(400);
+    }
+
+    [Test]
+    public void TestDivision()
+    {
+        // Arrange
+        var strExpression = "1900/2";
+
+        // Act
+        _expressionProcessor.GetOneExpression(strExpression);
+        var result = _expressionProcessor.Calculate();
+
+        // Assert
+        result.Should().Be(950);
+    }
+}
