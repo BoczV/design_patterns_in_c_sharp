55b14a262c62166a5259a155f36dd355dae84fbe
Bócz Viktor <56642389+BoczV@users.noreply.github.com>
2022-12-07 14:05:14 +0100

    Refactored example call in Program.cs

--- Facade/Program.cs
+++ Facade/Program.cs
@@ -1,16 +1,24 @@
 ﻿namespace Facade;
 
 using MagicSquare;
+using System.Text;
 
 public static class Program
 {
     static void Main()
     {
         List<List<int>> magicSquare = MagicSquareGenerator.Generate(4);
-        magicSquare.ForEach(
-            x => x.ForEach(
-                y => Console.WriteLine($"{magicSquare.IndexOf(x) + 1}. sublist, {x.IndexOf(y) + 1}. element is {y}.")
-                )
-            );
+        for (int i = 0; i < magicSquare.Count; i++)
+        {
+            Console.WriteLine("--------");
+            var subList = magicSquare[i];
+            var strBuilder = new StringBuilder('|');
+            for (int j = 0; j < subList.Count; j++)
+            {
+                strBuilder.Append(subList[j]).Append('|');
+            }
+            Console.WriteLine(strBuilder);
+        }
+        Console.WriteLine("--------");
     }
 }
