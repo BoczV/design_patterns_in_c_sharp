25764a60f0bd7bc161a99c291b20d6984584bcc6
Bócz Viktor <56642389+BoczV@users.noreply.github.com>
2022-12-07 13:33:24 +0100

    Tests added to Flyweight

---
+++ Structural.UnitTests/FacadeUnitTests.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Structural.UnitTests;
+
+[TestFixture]
+public class FacadeUnitTests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+}
---
+++ Structural.UnitTests/FlyWeightUnitTests.cs
@@ -0,0 +1,51 @@
+﻿using FluentAssertions;
+using Flyweight;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Structural.UnitTests;
+
+[TestFixture]
+public class FlyWeightUnitTests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void TestCapitalizationOfFirstWord()
+    {
+        // Arrange
+        var sentence = new Sentence("we are testing Flyweight!");
+
+        // Act
+        sentence[0].Capitalize = true;
+
+        // Assert
+        var stringSentence = sentence.ToString();
+        stringSentence.Should().Be("WE are testing Flyweight!");
+
+    }
+
+    [Test]
+    public void TestCapitalizationOfMultipleWord()
+    {
+        // Arrange
+        var sentence = new Sentence("we are testing Flyweight!");
+
+        // Act
+        sentence[0].Capitalize = true;
+        sentence[2].Capitalize = true;
+
+        // Assert
+        var stringSentence = sentence.ToString();
+        stringSentence.Should().Be("WE are TESTING Flyweight!");
+
+    }
+}
