00fbbc10ec8684809d795d337470e41b398bcf20
Viktor Bocz-Kemeny <beviktor95@gmail.com>
2023-10-21 12:11:16 +0200

    Fixes and new tests

--- src/Facade/MagicSquare/Components/Generator.cs
+++ src/Facade/MagicSquare/Components/Generator.cs
@@ -13,21 +13,16 @@ public static class Generator
 
     private static int GenerateRandomNumber(int minValue, int maxValue)
     {
-        if (minValue > maxValue)
-        {
-            throw new ArgumentException("minValue cannot be greater than maxValue");
-        }
-
         using var randomGenerator = RandomNumberGenerator.Create();
         var data = new byte[4];
         randomGenerator.GetBytes(data);
 
         // Convert the 4 bytes to an integer
-        int randomNumber = BitConverter.ToInt32(data, 0);
+        var randomNumber = BitConverter.ToInt32(data, 0);
 
         // Make sure the generated number is non-negative and within the specified range
-        int range = maxValue - minValue + 1;
-        int randomNonNegativeNumber = Math.Abs(randomNumber % range);
+        var range = maxValue - minValue + 1;
+        var randomNonNegativeNumber = Math.Abs(randomNumber % range);
 
         return randomNonNegativeNumber + minValue;
     }
